4e3a873fc1749a49aeacd939a3d31a7348b96e0e
P P <niezapominajek@gmail.com>
2019-06-11 18:36:06 +0200

    ...

---
+++ page.js
@@ -0,0 +1,23 @@
+$.getJSON("./data.json", function (data) {
+    var compl = [];
+    var act = [];
+    var done = "";
+    $.each(data, function (index, obj) {
+        if (obj.completed === false) {
+            done="unchecked";
+            act.push("<div class='form-check'><input class='form-check-input' type='checkbox' "+done+" value='' id='" + index + "' onchange='handleClick(this);' ><label class='form-check-label' for=" + index + ">"
+                + obj.name + "</label></div>");
+        } else {
+            done = "checked";
+            compl.push("<div class='form-check'><input class='form-check-input' type='checkbox'" + done + " value='' id='" + index + "'><label class='form-check-label' for=" + index + ">"
+                + obj.name + "</label></div>");
+        }
+    });
+
+    $("<ul/>", { html: compl.join("") }).appendTo("#compl, #all");
+    $("<ul/>", { html: act.join("") }).appendTo("#act, #all");
+});
+
+function handleClick(cb) {
+    console.log("Clicked, new value sr = " + cb.checked );
+}
